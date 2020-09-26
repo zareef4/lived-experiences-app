5aaff3baf8e6b442e90346e24740d132f74d24fd
acdhkl <dhakalavinav@alumni.ubc.ca>
2020-09-26 16:43:03 -0700

    Change icon structure, still need read more...

--- client/screens/Home.js
+++ client/screens/Home.js
@@ -1,7 +1,7 @@
 import React, { Component, useState } from "react";
 import Font, { useFonts } from "expo-font";
 import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right } from "native-base";
-import { FlatList, Image, StyleSheet } from "react-native";
+import { Image, StyleSheet } from "react-native";
 
 function HomeScreen({ navigation }) {
 	const [loaded] = useFonts({
@@ -17,61 +17,79 @@ function HomeScreen({ navigation }) {
 		<Container>
 			<Header>
 				<Left>
-					<Image source={require('../assets/logo.png')} style={{ width: 40, height: 40 }} />
+					<Button onPress={() => navigation.navigate("Home")} transparent>
+						<Image source={require('../assets/logo.png')} style={{ width: 40, height: 40 }} />
+					</Button>
 				</Left>
 
 				<Right>
 					{/* SEARCH */}
 					<Button transparent>
-						<Icon name="search" style={{ padding: 2, color: "black" }}></Icon>
+						<Icon onPress={() => navigation.navigate("Search")} name="search" style={styles.reactIcons}></Icon>
 					</Button>
 					{/* FILTER */}
-					<Button transparent>
-						<Icon name="md-options" style={{ padding: 2, color: "black" }}></Icon>
+					<Button onPress={() => navigation.navigate("Filter")} transparent>
+						<Icon name="md-options" style={styles.reactIcons}></Icon>
 					</Button>
 					{/* MENU */}
-					<Button transparent>
-						<Icon name="md-menu" style={{ padding: 2, color: "black" }}></Icon>
+					<Button onPress={() => navigation.navigate("Menu")} transparent>
+						<Icon name="md-menu" style={styles.reactIcons}></Icon>
 					</Button>
 				</Right>
 			</Header>
 
-
-			<Content>
-				<Card>
-					<CardItem>
-					</CardItem>
-					{/* POST IMAGE */}
-					<CardItem cardBody>
-						<Image source={{ uri: 'https://www.rollingstone.com/wp-content/uploads/2018/06/inside-black-lives-matter-33f06033-8048-496e-b3c1-5290a6d9c5b8.jpg?w=1024' }} style={{ height: 200, width: null, flex: 1 }} />
-					</CardItem>
-					<CardItem>
-						<Body>
-							{/* POST TITLE */}
-							<Text style={styles.postTitle}>POST TITLE</Text>
-							{/* POST AUTHOR */}
-							<Text note>
-								By: <Text style={styles.iosBrown}>Sean Bowles</Text>
-							</Text>
-						</Body>
-						<Right>
-							<Button iconLeft transparent>
-								{/* ONCLICK: LIKE */}
-								<Icon name="md-heart" />
-							</Button>
-						</Right>
-					</CardItem>
-					<CardItem>
-						{/* POST BODY */}
-						<Text>
-							Bacon ipsum dolor amet chuck bresaola pork chop pancetta, spare
-							ribs short loin swine frankfurter burgdoggen turkey landjaeger.
-							Porchetta pork belly short ribs spare ribs...
+			<Container>
+				<Content>
+					<Card>
+						<CardItem>
+						</CardItem>
+						{/* POST IMAGE */}
+						<CardItem cardBody>
+							<Image source={{ uri: 'https://www.rollingstone.com/wp-content/uploads/2018/06/inside-black-lives-matter-33f06033-8048-496e-b3c1-5290a6d9c5b8.jpg?w=1024' }} style={{ height: 200, width: null, flex: 1 }} />
+						</CardItem>
+						<CardItem>
+							<Body>
+								{/* POST TITLE */}
+								<Text style={styles.postTitle}>POST TITLE</Text>
+								{/* POST AUTHOR */}
+								<Text note>
+									By: <Text style={styles.iosBrown}>Sean Bowles</Text>
+								</Text>
+							</Body>
+						</CardItem>
+						<CardItem>
+							{/* POST BODY */}
+							<Text>
+								Bacon ipsum dolor amet chuck bresaola pork chop pancetta, spare
+								ribs short loin swine frankfurter burgdoggen turkey landjaeger.
+								Porchetta pork belly short ribs spare ribs...
+								 {/* NEED HELP IMPLEMENTING READ MORE BUTTON */}
       					</Text>
-					</CardItem>
-				</Card>
-			</Content>
-
+						</CardItem>
+						<CardItem>
+							<Left>
+								{/* LOVE REACTION */}
+								<Button transparent>
+									<Icon name="md-heart" style={styles.reactIcons}></Icon>
+								</Button>
+								{/* HAPPY REACTION */}
+								<Button transparent>
+									<Icon name="md-thumbs-up" style={styles.reactIcons}></Icon>
+								</Button>
+								{/* SAD REACTION*/}
+								<Button transparent>
+									<Icon name="md-thumbs-down" style={styles.reactIcons}></Icon>
+								</Button>
+							</Left>
+							<Right>
+								<Button transparent>
+									<Icon name="md-share" style={styles.reactIcons}></Icon>
+								</Button>
+							</Right>
+						</CardItem>
+					</Card>
+				</Content>
+			</Container>
 		</Container>
 
 
@@ -82,13 +100,16 @@ const styles = StyleSheet.create({
 		flex: 1
 	},
 	iosBrown: {
-		fontFamily: "roboto-regular",
 		color: "rgba(230,179,0,1)",
 		fontWeight: "400"
 	},
 	postTitle: {
 		fontSize: 32,
 		fontFamily: "Times New Roman"
+	},
+	reactIcons: {
+		padding: 2,
+		color: "black"
 	}
 });
 
