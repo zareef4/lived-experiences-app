e74d0d7fe1aed81583d6bf8181ed6fb04cb13702
Zareef Baksh <zbak@tugo.com>
2020-09-26 11:14:35 -0700

    added navigation, readme and 2 example screens

--- client/App.js
+++ client/App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import * as React from "react";
+import { View, Text, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./screens/Home";
+import DetailsScreen from "./screens/Details";
 
-export default function App() {
+const Stack = createStackNavigator();
+
+function App() {
 	return (
-		<View style={styles.container}>
-			<Text>Hello... Starter for Vanhacks App</Text>
-			<StatusBar style="auto" />
-		</View>
+		<NavigationContainer>
+			<Stack.Navigator initialRouteName="Home">
+				<Stack.Screen name="Home" component={HomeScreen} />
+				<Stack.Screen name="Details" component={DetailsScreen} />
+			</Stack.Navigator>
+		</NavigationContainer>
 	);
 }
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center"
-	}
-});
+export default App;
---
+++ client/README.md
@@ -0,0 +1,8 @@
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Starts the Expo Server (includes instructions on running the app)<br />
+Open [http://localhost:19002](http://localhost:19002) to view the dev tools in the browser.
