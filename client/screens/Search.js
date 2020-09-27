1e6bd053a42f73ae64ff7c25182ac4a3f13f7dd4
Zareef Baksh <zbak@tugo.com>
2020-09-27 08:58:08 -0700

    added styling to search

--- client/screens/Search.js
+++ client/screens/Search.js
@@ -1,8 +1,21 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Font, { useFonts } from "expo-font";
-import { Container, Text, Button } from "native-base";
+import { Image } from "react-native";
+import {
+	Container,
+	Header,
+	Left,
+	Body,
+	Right,
+	Button,
+	Text,
+	Content,
+	Form,
+	Item,
+	Input
+} from "native-base";
 
-function SearchScreen() {
+function SearchScreen({ navigation }) {
 	const [loaded] = useFonts({
 		Roboto: require("native-base/Fonts/Roboto.ttf"),
 		Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
@@ -13,10 +26,32 @@ function SearchScreen() {
 	}
 
 	return (
-		<Container
-			style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-		>
-			<Text>Search Screen</Text>
+		<Container>
+			<Header>
+				<Left>
+					<Button
+						transparent
+						onPress={() => {
+							navigation.goBack();
+						}}
+					>
+						<Image source={require("../assets/xIcon.png")} />
+					</Button>
+				</Left>
+				<Right></Right>
+			</Header>
+			<Content>
+				<Form style={{ paddingRight: 20 }}>
+					<Text
+						style={{ marginLeft: 20, fontSize: 32, marginTop: 100 }}
+					>
+						Find Experiences
+					</Text>
+					<Item>
+						<Input style={{ fontSize: 32, fontWeight: "600" }} />
+					</Item>
+				</Form>
+			</Content>
 		</Container>
 	);
 }
